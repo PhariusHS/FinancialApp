c5dccc9b23c58a508943289094564f7392d67b9b
PhariusHS <lucaponteprimo04@gmail.com>
2024-04-10 17:19:59 +0200

    Auth start

--- server/server.js
+++ server/server.js
@@ -3,16 +3,17 @@ import express from "express";
 import bodyParser from "body-parser";
 import spentsRoutes from "./routes/spent.routes.js";
 import cors from "cors";
+import morgan from "morgan";
+import authRoutes from './routes/auth.routes.js'
 
 const PORT = 3000;
 
 const app = express();
+app.use(morgan("dev"));
 
 // Set baseUrl based on environment or configuration
 const isAndroid = process.env.PLATFORM === "android";
-const baseUrl = isAndroid
-  ? "192.168.40.76"
-  : "localhost";
+const baseUrl = isAndroid ? "192.168.40.76" : "localhost";
 
 app.use(
   cors({
@@ -29,4 +30,6 @@ app.listen(PORT, () => {
 
 connectDB();
 
+
 app.use("/api", spentsRoutes);
+app.use("/api", authRoutes);
